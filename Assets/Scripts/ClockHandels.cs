dcf841e874fc3c9788c5e796ada52cabb5012856
saeedsafikhani <saiedbpcc@gmail.com>
2019-03-02 11:26:52 +0100

    Clock_Creation_02032019

---
+++ Assets/Scripts/ClockHandels.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockHandels : MonoBehaviour
+{
+    private const float
+    hoursToDegrees = 360f / 12f,
+    minutesToDegrees = 360f / 60f,
+    secondsToDegrees = 360f / 60f;
+    float AccuSecond = 0;
+    public Transform Hours, Minutes, Seconds;
+    public Transform Handle01,Handle02,Handle03,Handle04,Handle05,Handle06,Handle07,Handle08;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        AccuSecond = AccuSecond + Time.deltaTime;        
+        //Hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+        //Minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+        //Seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
+        Hours.localRotation = Quaternion.Euler(-90f, 0f, time.Hour * hoursToDegrees);
+        Minutes.localRotation = Quaternion.Euler(-90f, 0f, time.Minute * minutesToDegrees);
+        Seconds.localRotation = Quaternion.Euler(-90f, 0f, time.Second * secondsToDegrees);
+        Handle01.localRotation = Quaternion.Euler(-90f, 0f, time.Second * -secondsToDegrees);
+        Handle02.localRotation = Quaternion.Euler(-90f, 0f, time.Second * secondsToDegrees);
+        Handle03.localRotation = Quaternion.Euler(-90f, 0f, AccuSecond * -secondsToDegrees);
+        Handle04.localRotation = Quaternion.Euler(-90f, 0f, AccuSecond * secondsToDegrees);
+        Handle05.localRotation = Quaternion.Euler(-90f, 0f, AccuSecond * -secondsToDegrees);
+        Handle06.localRotation = Quaternion.Euler(-90f, 0f, time.Second * secondsToDegrees);
+        Handle07.localRotation = Quaternion.Euler(-90f, 0f, AccuSecond * -secondsToDegrees);
+        Handle08.localRotation = Quaternion.Euler(-90f, 0f, time.Second * -secondsToDegrees);
+    }
+}
